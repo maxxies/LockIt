82756273dbc716d690e44f883a78bdfdae42f09b
maxxies <ahiamadzormaxwell7@gmail.com>
2024-07-27 19:56:44 +0000

    Update function for confirming room

--- main.py
+++ main.py
@@ -9,41 +9,31 @@ load_dotenv()
 # Set up app
 app = FastAPI()
 
-APIKEY = os.getenv("APIKEY")
-AUTHDOMAIN = os.getenv("AUTHDOMAIN")
-DATABASEURL = os.getenv("DATABASEURL")
-PROJECTID = os.getenv("PROJECTID")
-STORAGEBUCKET = os.getenv("STORAGEBUCKET")
-MESSAGINGSENDERID = os.getenv("MESSAGINGSENDERID")
-APPID = os.getenv("APPID")
-MEASUREMENTID = os.getenv("MEASUREMENTID")
-
-# Firebase configuration
-firebaseConfig = {
-  "apiKey": APIKEY,
-  "authDomain": AUTHDOMAIN,
-  "databaseURL": DATABASEURL,
-  "projectId": PROJECTID,
-  "storageBucket": STORAGEBUCKET,
-  "messagingSenderId": MESSAGINGSENDERID,
-  "appId": APPID,
-  "measurementId": MEASUREMENTID
+# Retrieve Firebase configuration from environment variables
+firebase_config = {
+    "apiKey": os.getenv("APIKEY"),
+    "authDomain": os.getenv("AUTHDOMAIN"),
+    "databaseURL": os.getenv("DATABASEURL"),
+    "projectId": os.getenv("PROJECTID"),
+    "storageBucket": os.getenv("STORAGEBUCKET"),
+    "messagingSenderId": os.getenv("MESSAGINGSENDERID"),
+    "appId": os.getenv("APPID"),
+    "measurementId": os.getenv("MEASUREMENTID")
 }
 
-# Set up database
-firebase = pyrebase.initialize_app(firebaseConfig)
+# Initialize Firebase
+firebase = pyrebase.initialize_app(firebase_config)
 auth = firebase.auth()
 db = firebase.database()
 
-
 @app.get("/confirm_room")
-async def confirm(card: str):
-    # Get the allowed rooms
-    room, numbers  = db.child("alloweRooms").get().pyres[0].item
+async def confirm_room(card: str):
+    # Retrieve the allowed rooms
+    allowed_rooms = db.child("alloweRooms").get().val()
 
-    # Check if the card is allowed for room
-    if card in numbers:
-        return True
+    # Check if the card is allowed for any room
+    for room, numbers in allowed_rooms.items():
+        if card in numbers:
+            return {"room": room, "access": True}
 
-    return False
-    
+    return {"room": None, "access": False}
